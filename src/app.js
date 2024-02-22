c0a717124c34136b070d5696fa8c5130723cf1c0
Javier Diez <javierdiez92@gmail.com>
2024-02-22 15:15:31 +0000

    Generador de excusas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,45 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function excuse() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let who_random = Math.floor(Math.random() * who.length);
+  let who_random_result = who[who_random];
+  let action_random = Math.floor(Math.random() * action.length);
+  let action_random_result = action[action_random];
+  let what_random = Math.floor(Math.random() * what.length);
+  let what_random_result = what[what_random];
+  let when_random = Math.floor(Math.random() * when.length);
+  let when_random_result = when[when_random];
+  console.log(
+    who_random_result +
+      " " +
+      action_random_result +
+      " " +
+      what_random_result +
+      " " +
+      when_random_result
+  );
+  return (
+    who_random_result +
+    " " +
+    action_random_result +
+    " " +
+    what_random_result +
+    " " +
+    when_random_result
+  );
+}
+document.addEventListener("DOMContentLoaded", function() {
+  let resultadoExcuse = document.getElementById("resultadoExcuse");
+  resultadoExcuse.textContent = excuse();
+});
